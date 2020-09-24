09e887b293c361ff40ef1c14e07562502a4a090e
Famil <frostgamingrespawn@gmail.com>
2020-09-24 14:08:29 +0200

    text filed related functions added

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,45 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTextField.delegate = self;
     }
 
 
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true);
+    }
+    
+    //keyboard return button pressed
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.endEditing(true);
+        return true;
+    }
+    
+    //Runs when user clicks somewhere else in the app
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true;
+        }else {
+            textField.placeholder = "Type something here";
+            return false;
+        }
+    }
+    
+    //Runs after user done with editing text field
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.text = "";
+    }
 }
 
